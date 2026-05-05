2d5f82e0d8f91b60834afe96d5447dfac4ac3e48
JPaulus04 <joshua.r.paulus@gmail.com>
2026-05-04 21:14:18 -0400

    Add files via upload

---
+++ src/game/GameConfig.js
@@ -0,0 +1,21 @@
+import Phaser from 'phaser';
+import { BootScene } from './scenes/BootScene';
+import { PreloadScene } from './scenes/PreloadScene';
+import { WorldScene } from './scenes/WorldScene';
+import { UIScene } from './scenes/UIScene';
+
+export const GameConfig = {
+  type: Phaser.AUTO,
+  backgroundColor: '#1a1a2e',
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 390,
+    height: 844,
+  },
+  physics: {
+    default: 'arcade',
+    arcade: { gravity: { y: 0 }, debug: false },
+  },
+  scenes: [BootScene, PreloadScene, WorldScene, UIScene],
+};
